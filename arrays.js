e4496ab52ee26e7607a5ff2ac2854d3b9f2503d5
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-16 00:39:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = new Array();
 var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
-var addElementToBeginningOfArray;(array, element)
+var addElementToBeginningOfArray= new Array(array, element)
 var addElementToBeginningOfArray = ['adds an element to the beginning of an array']
